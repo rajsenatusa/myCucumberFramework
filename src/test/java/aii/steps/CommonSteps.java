5cd3dafbbf358cd3db6685ec0a7e3a97cea95b79
Prafull Vispute <pvispute@aiiflorida.com>
2023-03-13 10:12:29 -0400

    updated

--- src/test/java/aii/steps/CommonSteps.java
+++ src/test/java/aii/steps/CommonSteps.java
@@ -1,9 +1,13 @@
 package aii.steps;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import aii.utils.CommonMethods;
 import aii.utils.ConfigsReader;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -11,16 +15,16 @@ import io.cucumber.java.en.When;
 public class CommonSteps extends CommonMethods {
 
 	@Given("I signin Spin as Standard Agent")
-	public void i_signin_spin_as_standard_agent() {
+	public void i_signin_spin_as_standard_agent_common() {
 
 	 sendText(login.username, ConfigsReader.getProperty("username"));
 	 sendText(login.password, ConfigsReader.getProperty("password"));
 	 click(login.btnSignIn);
-	 wait(3);	
-		
+	 wait(3);		
 	}
-	@Given("I start transaction as a new customer")
-	public void i_start_transaction_as_a_new_customer() {
+	
+	@Given("I start transaction as a New Customer")
+	public void i_start_transaction_as_a_new_customer_common() {
 			   
 		wait(1);
 		moveToElement(driver.findElement(By.id("Menu_Policy")));
@@ -32,7 +36,7 @@ public class CommonSteps extends CommonMethods {
 	}
 	
 	@When("I select the product from Product Selection List as {string}")
-	public void i_enter_product_selection_information(String Product) {
+	public void i_enter_prod_selection_information_common(String Product) {
 		//product selection information
 		switch (Product) {
 		case "HO3":
@@ -48,7 +52,52 @@ public class CommonSteps extends CommonMethods {
 		
 		}
 	
+	@And("I change date of system {string}")
+	public void i_validate_change_Date(String date) throws Exception {
+		 click(dashboard.btnAdmin);
+		 click(dashboard.btnChangeDate);
+		 sendText(dashboard.txtNewDate, date);
+		 click(dashboard.btnChangeNewDate);
+		 sendText(dashboard.txtNewBookDate, date);
+		 click(dashboard.btnChangeBookDate);
+	}
+	
+	@And("I enter Quote Information as effective date with {string} days difference and state {string} and {string} Insurance Carrier group")
+	public void i_enter_quote_Information(String Days, String State, String CarrierGroup) throws Exception {
 		
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM/dd/yyy");
+		LocalDateTime currentDate = LocalDateTime.now();
+		
+		String effectiveDate = dtf.format(currentDate.plusDays(Integer.parseInt(Days)));	
+		
+		// Change system date to effective date
+		 click(dashboard.btnAdmin);
+		 click(dashboard.btnChangeDate);
+		 sendText(dashboard.txtNewDate, effectiveDate);
+		 click(dashboard.btnChangeNewDate);
+		 sendText(dashboard.txtNewBookDate, effectiveDate);
+		 click(dashboard.btnChangeBookDate);
+		
+		click(dashboard.lnkNewQuote);
+		
+		sendText(dashboard.txtEffectiveDate,effectiveDate);
+		
+		if (State.toUpperCase().contains("FL") || State.toUpperCase().contains("FLORIDA") ) {
+			selectDropdown(dashboard.ddState, 1);
+		}
+		
+		if (CarrierGroup.toUpperCase().contains("AI") || State.toUpperCase().contains("AMERICAN INTEGRITY INSURANCE GROUP") ) {
+			selectDropdown(dashboard.ddCarrierGroup, 1);
+		}
+				
+		wait(2);
+		click(dashboard.btnNewQuoteStart);
+		
+
+			
+		
+		
+	}
 
 
 
