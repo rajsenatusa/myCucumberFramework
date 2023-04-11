1f7c43259770bb39a28c0a35186305aa1db63601
Prafull Vispute <pvispute@aiiflorida.com>
2023-04-11 04:11:55 -0400

    Added

--- src/test/java/aii/steps/PasswordReset.java
+++ src/test/java/aii/steps/PasswordReset.java
@@ -1,32 +1,6 @@
 package aii.steps;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.lang.reflect.Array;
-import java.time.LocalDateTime;
-import java.util.concurrent.TimeUnit;
-
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.UnhandledAlertException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-
 import aii.utils.CommonMethods;
-import aii.utils.ConfigsReader;
 import aii.utils.ExcelUtility;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
@@ -35,16 +9,16 @@ import io.cucumber.java.en.When;
 
 
 
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
+//import org.openqa.selenium.support.FindBy;
+//import org.openqa.selenium.support.PageFactory;
 
 
 
 public class PasswordReset extends CommonMethods {
 	String FileLocation = System.getProperty("user.dir") + "\\src\\test\\resources\\testdata\\UserList_ToRest.xlsx";
 	
-	@Then ("I given all test user list to reset the password to {string}")
-	public void i_given_all_test_user_list_to_reset_the_password_to(String password) {
+	@Then ("I given all test user list to reset the password")
+	public void i_given_all_test_user_list_to_reset_the_password_to() {
 		String user="test user";
 		Object[][] listofUsers;
 		
@@ -70,7 +44,7 @@ public class PasswordReset extends CommonMethods {
 		sendText(userLookup.txtPassword,listofUsers[j][1].toString());
 		sendText(userLookup.txtConfirmPassword,listofUsers[j][1].toString());
 		click(userLookup.chkPasswordChangeNextLogin);
-//		click(userLookup.btnChangePassword);
+		click(userLookup.btnChangePassword);
 		System.out.println("Password Changed Successfully for user -" + listofUsers[j][0].toString());
 		}
 		} catch (Exception  e) {
